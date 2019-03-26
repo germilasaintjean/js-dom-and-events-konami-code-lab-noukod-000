a3120f322db48ec2aba6836c87a246c58fc6a641
Saint-Jean Germila <germila@noukod.com>
2019-03-26 17:25:32 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,15 +2,15 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // let index = 0
-  // 
+  //
   // document.body.addEventListener('keydown', function(event) {
   //   const key = parseInt(event.detail || event.which);
-  // 
+  //
   //   if (key === code[index]) {
   //     index++;
   //     if (index === code.length) {
   //       alert("Hurray!");
-  // 
+  //
   //       index = 0;
   //     }
   //   } else {
